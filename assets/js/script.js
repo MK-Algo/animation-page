798890adb7afd238347e6851217d4e6b105720a7
loveagile <happyplandry@gmail.com>
2024-09-20 21:04:42 +0900

    feat: initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(".slider").slick({
+  draggable: false,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  arrows: false,
+  dots: false,
+  fade: true,
+  speed: 4000,
+  infinite: true,
+  cssEase: "ease-out",
+  touchThreshold: 100,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+});
